f4211f168f9d6579a10576886e6aac03dbd9db24
LAKSHMANA <lv@stylelabs.com>
2014-12-29 13:26:36 +0100

    restructure

---
+++ src/todo.web.app/Scripts/app/tasksViewModel.js
@@ -0,0 +1,23 @@
+﻿(function(todo) {
+
+    todo.TasksViewModel = function () {
+        var self = this;
+
+        self.tasks = ko.observableArray();
+        self.addTask = function() {
+            self.tasks.push(new todo.Task(self.taskDescription()));
+        };
+
+        self.removeTask = function() {
+            self.tasks.remove(this);
+        };
+
+        self.taskDescription = ko.observable();
+
+        self.dateTimeNow = ko.observable(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        self.updateClock = function() {
+            self.dateTimeNow(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        };
+    };
+
+})(window.todo = window.todo || {});
